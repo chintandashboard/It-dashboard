d3679be7ba7746347cbbccd91dfa74df8b73c784
Manishgupta-coder <guptamanish2023.gmail.com>
2026-04-08 09:41:01 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/chintandashboard/",
+  base: "/It-dashboard/",
   server: {
     host: "::",
     port: 8080,
